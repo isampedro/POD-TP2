24c69a7cf8edb0d92a014be0c568735dbb18b25d
Rolo2796 <arolandelli@itba.edu.ar>
2021-10-27 18:57:00 -0300

    TESTS

--- api/src/test/java/ar/edu/itba/pod/api/QueryTest.java
+++ api/src/test/java/ar/edu/itba/pod/api/QueryTest.java
@@ -1,6 +1,10 @@
 package ar.edu.itba.pod.api;
 
+import java.util.*;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import org.junit.After;
 import org.junit.Test;
 
@@ -30,9 +34,52 @@ public class QueryTest {
     }
 
 
+    //TODO: CHECK CSV
+
+    private static final List<Tree> trees = Arrays.asList(
+            new Tree("a", new Neighborhood("40", 6), "Gral Wololo"),
+            new Tree("b", new Neighborhood("40", 6), "Gral Wololo"),
+            new Tree("c", new Neighborhood("40", 6), "Gral Wololo"),
+            new Tree("d", new Neighborhood("40", 6), "Gral Wololo"),
+            new Tree("e", new Neighborhood("11", 2), "Av jusepe"));
+
+
+    //Total de árboles por barrio
     @Test
     public void query1Test() throws InterruptedException, ExecutionException {
 
+        HazelcastInstance h = Hazelcast.newHazelcastInstance();
+
+        IList<Tree> iTrees = h.getList("treeTEST");
+        iTrees.addAll(trees);
+
+        final JobTracker tracker = h.getJobTracker("query1");
+
+        KeyValueSource<String, Tree> sourceTrees = KeyValueSource.fromList(iTrees);
+
+        Job<String, Tree> job = tracker.newJob(sourceTrees);
+        ICompletableFuture<Map<String, Long>> future = job
+                .mapper(new Query1Mapper())
+                .combiner(new Query1CombinerFactory())
+                .reducer(new SumReducerFactory())
+                .submit();
+
+        Map<String, Long> rawResult = future.get();
+        List<String> outLines = postProcess(rawResult);
+
+        assertEquals(2, outLines.size());
+        assertEquals(outLines.get(2),  "40;4");
+        assertEquals(outLines.get(1),"11;1");
+    }
+
+    private static List<String> postProcess(Map<String, Long> rawResult) {
+        List<Map.Entry<String, Long>> result = rawResult.entrySet().stream()
+                .sorted(Comparator.comparing((Function<Map.Entry<String, Long>, Long>) Map.Entry::getValue)
+                        .thenComparing(Map.Entry::getKey)).collect(Collectors.toList());
+
+        return result.stream()
+                .map(entry -> entry.getKey() + ";" + entry.getValue())
+                .collect(Collectors.toList());
     }
 
 }
